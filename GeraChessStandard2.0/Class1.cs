eff6ebbe003b32a270ed98da4df04640e5fd334b
NISHANTH DURAI <nishanthduraisamy@gmail.com>
2023-03-19 21:23:15 +0530

    initial-commit

---
+++ GeraChessStandard2.0/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace customChessLibrary
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            var chess = new ChessBoard();
+            Console.WriteLine(chess.ToAscii());
+        }
+    }
+}
